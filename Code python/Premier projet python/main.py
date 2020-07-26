1a5927472b310e21aff259cf5e3745d2b28f2aed
NewFrenchDev <68789915+NewFrenchDev@users.noreply.github.com>
2020-07-26 19:49:56 +0200

    new class mobs created

--- Code python/Premier projet python/main.py
+++ Code python/Premier projet python/main.py
@@ -1,12 +1,13 @@
 from player import Player
 from player import Sorcerer
+from mobs import Monster
+from mobs import Gobelin
 
 def main():
-    Player1 = Player("Ailios", 30, 8)
-    Player2 = Sorcerer("Hermes", 20, 6)
-    Player1.attack_player(Player2)
-    Player1.attack_player(Player2)
-    Player1.attack_player(Player2)
+    Player1 = Player("Ailios", 1, 10, 30, 8)
+    Player2 = Sorcerer("Hermes", 1, 10, 20, 6)
+    Monster1 = Gobelin(10, 2, 2)
+
 
 if __name__ == '__main__':
     main()
