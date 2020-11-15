229a259739003aa31ae1c1cfd97dee806c95eee4
NewFrenchDev <68789915+NewFrenchDev@users.noreply.github.com>
2020-11-15 13:40:35 +0100

    Update button.py
    New fonction added
    
    click_on_button which permit to launch a function when any part of the button is clicked
    
    is_over that permit to know if the mouse is on the button or not

--- Code python/Quizz Game with graphic/button.py
+++ Code python/Quizz Game with graphic/button.py
@@ -1,14 +1,16 @@
 import pygame
 
-class Button:
 
-    def __init__(self, color=[0, 0, 0], x=0, y=0, width=0, height=0, text=''):
+class Button(pygame.Rect):
+
+    def __init__(self, color=[133, 133, 133], x=0, y=0, width=0, height=0, text='', action=None):
         self.color = color
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.text = text
+        self.action = action
 
     def draw(self, screen, outline=None):
         if outline:
@@ -19,4 +21,22 @@ class Button:
         if self.text != "":
             font = pygame.font.SysFont("comicsans", 20)
             text = font.render(self.text, 1, (0, 0, 0))
-            screen.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+            screen.blit(text, (self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+
+    def is_over(self, pos):
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
+                return True
+
+        return False
+
+    def click_on_button(self):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if self.x + self.width > mouse[0] > self.x and self.y + self.height > mouse[1] > self.y:
+            if click[0] == 1:
+                self.action()
+
+    def display(self):
+        print(f"Button {self.text} pressed")
